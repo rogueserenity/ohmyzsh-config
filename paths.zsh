216c2a2502e4b7c7a7353f0d79c7985894e5d743
Jay Burgess <jragingfury@gmail.com>
2020-11-10 18:28:05 -0700

    fix path instead of calculating it

--- paths.zsh
+++ paths.zsh
@@ -1,4 +1,1 @@
-for APP_PATH in /opt/*/bin
-do
-	export PATH=${APP_PATH}:${PATH}
-done
+export PATH=/opt/apache-maven-3.6.3/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/MacGPG2/bin:/Library/Apple/usr/bin
