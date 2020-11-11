9a77e1ff3671bff2e8c6008d72131addf2925f5d
Jay Burgess <jragingfury@gmail.com>
2020-11-10 17:35:30 -0700

    update to support zsh loading order
    write function to remove any java from path before adding new java to path

--- java.zsh
+++ java.zsh
@@ -1,5 +1,19 @@
-export JAVA_HOME=`/usr/libexec/java_home`
-export PATH=${PATH}:${JAVA_HOME}/bin
-alias j8='export JAVA_HOME=`/usr/libexec/java_home -v 1.8`;java -version'
-alias j11='export JAVA_HOME=`/usr/libexec/java_home -v 11`;java -version'
-alias j15='export JAVA_HOME=`/usr/libexec/java_home -v 15`;java -version'
+
+function remove_java_path() {
+  set -o shwordsplit
+  LOOP_PATH=`echo ${PATH} | tr ":" " "`
+  TMP_PATH=""
+  for ENTRY in $LOOP_PATH
+  do
+    if [[ $ENTRY != /Library/Java/* ]]
+    then
+      TMP_PATH=${TMP_PATH}:${ENTRY}
+    fi
+  done
+  export PATH=`echo ${TMP_PATH} | sed s/^://`
+}
+
+#JAVA VERSION ALIASES
+alias j8='remove_java_path;export JAVA_HOME=`/usr/libexec/java_home -v 1.8`;export PATH=${PATH}:${JAVA_HOME}/bin;java -version'
+alias j11='remove_java_path;export JAVA_HOME=`/usr/libexec/java_home -v 11`;export PATH=${PATH}:${JAVA_HOME}/bin;java -version'
+alias j15='remove_java_path;export JAVA_HOME=`/usr/libexec/java_home -v 15`;export PATH=${PATH}:${JAVA_HOME}/bin;java -version'
