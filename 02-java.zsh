c085e16df7c985b744c4152adf0dcac21209742d
Jay Burgess <jragingfury@gmail.com>
2023-04-30 11:17:20 -0600

    update java setup; default to java 17

--- 02-java.zsh
+++ 02-java.zsh
@@ -13,8 +13,22 @@ function remove_java_path() {
   export PATH=`echo ${TMP_PATH} | sed s/^://`
 }
 
+function set_java_version() {
+  VERSION=$1
+  remove_java_path
+  export JAVA_HOME=/Library/Java/JavaVirtualMachines/${VERSION}/Contents/Home
+  export PATH=${JAVA_HOME}/bin:${PATH}
+}
+
+function set_java_and_print() {
+  set_java_version $1
+  java -version
+}
+
 #JAVA VERSION ALIASES
-alias j8='remove_java_path;export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_202.jdk/Contents/Home;export PATH=${JAVA_HOME}/bin:${PATH};java -version'
-alias j11='remove_java_path;export JAVA_HOME=/Library/Java/JavaVirtualMachines/temurin-11.jdk/Contents/Home;export PATH=${JAVA_HOME}/bin:${PATH};java -version'
-alias j17='remove_java_path;export JAVA_HOME=/Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home;export PATH=${JAVA_HOME}/bin:${PATH};java -version'
-alias j20='remove_java_path;export JAVA_HOME=/Library/Java/JavaVirtualMachines/temurin-20.jdk/Contents/Home;export PATH=${JAVA_HOME}/bin:${PATH};java -version'
+alias j8='set_java_and_print jdk1.8.0_202.jdk'
+alias j11='set_java_and_print temurin-11.jdk'
+alias j17='set_java_and_print temurin-17.jdk'
+alias j20='set_java_and_print temurin-20.jdk'
+
+set_java_version temurin-17.jdk
